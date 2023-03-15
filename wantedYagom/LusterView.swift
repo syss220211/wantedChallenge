7fb25af741b34367ae4c878ae3ab71c326ba06a1
syss220211 <syss220211@gmail.com>
2023-03-16 00:53:29 +0900

    옵저빙을 위한 workItem 생성 및 취소

--- wantedYagom/LusterView.swift
+++ wantedYagom/LusterView.swift
@@ -31,8 +31,13 @@ final class LusterView: UIView {
     
     // 프로그래스 바 설정
     private var observation: NSKeyValueObservation!
+    
     // 버튼 stop 표시
     private var task: URLSessionDataTask!
+    
+    // datatask가 아니라 dispatch의 작업 자체를 취소하기 위해서 (dispatch의 작업 단위 = Item)
+    private var workItem: DispatchWorkItem!
+    
     
     override func awakeFromNib() {
         super.awakeFromNib()
@@ -42,27 +47,92 @@ final class LusterView: UIView {
     }
     
     deinit {
-        observation.invalidate()
+        observation?.invalidate()
         observation = nil
     }
     
     func reset() {
-        imageView.image = .init(systemName: "photo")
-        progressView.progress = 0
-//        loadButton.isEnabled = true
-        loadButton.isSelected = false
+        DispatchQueue.main.async {
+            self.imageView.image = .init(systemName: "photo")
+            self.progressView.progress = 0
+    //        loadButton.isEnabled = true
+            self.loadButton.isSelected = false
+        }
+        
     }
     
     func loadImage() {
         loadButton.sendActions(for: .touchUpInside)
     }
     
+    // GCP에서 취소하기 ( != cancel)
+    func stopLoading() {
+        
+    }
+    
+    private func startLoad(url: URL) {
+        
+        workItem = DispatchWorkItem {
+            let request = URLRequest(url: url)
+            
+            self.task = URLSession.shared.dataTask(with: request) { data, response, error in // 이미지 다운로드 끝나면 {} 실행
+                if let error = error {
+                    guard error.localizedDescription == "cancelled" else {
+                        fatalError(error.localizedDescription)
+                    }
+                    DispatchQueue.main.async {
+                        self.reset()
+                    }
+                    return
+                }
+                // 여기까지 다 main thread에서 작업 (화면에서 조작 = main thread에서 작업)
+                guard let data = data, let image = UIImage(data: data) else {
+                    DispatchQueue.main.async {
+                        // 이미지 다운 받고, 화면에 그리는 2개의 작업을 하기 때문에 동시성 사용, road all은 화면 5개를 그려야함
+                        // main을 통해서 이미지 다 다운 받았으니 화면에 그리라는 일을 전달 해야 하는 데 화면 UI 그리는일은 main thread에서만 가능하기 때문에 .main.async에서 imageView 그려주기
+                        self.imageView.image = .init(systemName: "xmark") // 일을 보낸쪽으로 다시 되돌려주어야하는 부분도 있기 때문에 되돌림
+                    }
+                    return
+                }
+                
+                guard self.workItem.isCancelled == false else {
+                    self.reset() // cancel 이라면 reset 후 return
+                    return
+                }
+                
+                DispatchQueue.main.async {
+                    self.imageView.image = image
+                    self.loadButton.isSelected = false
+                }
+            }
+            
+            self.observation = self.task.progress.observe(\.fractionCompleted, options: [.new], changeHandler:  { progress, change in
+                DispatchQueue.main.async {
+                    // stop 눌렀을 때 옵저빙도 같이 stop 되도록 guard
+                    guard self.workItem.isCancelled == false else {
+                        // workItem이 cancel이라면,
+                        self.observation.invalidate()
+                        self.observation = nil
+                        self.progressView.progress = 0
+                        return
+                    }
+                    self.progressView.progress = Float(progress.fractionCompleted)
+                    // task의 진행상황 옵저빙(progress->fractionCompleted)
+                }
+            })
+            self.task.resume() // Datatask를 새로운 백그라운드 쓰레드에서 샐행시켜줌 -> 이미지 다운로드 작업 진행
+        }
+        DispatchQueue.global().async(execute: workItem)
+    }
+    
     // button 을 누르면 로드 되는 함수
     @IBAction private func touchUpLoadButton(_ sender: UIButton) {
         
-        sender.isSelected.toggle()
+        sender.isSelected = !sender.isSelected
+        
         guard sender.isSelected else {
-            task.cancel()
+//            task.cancel()
+            self.workItem.cancel()
             return
         }
         
@@ -73,40 +143,6 @@ final class LusterView: UIView {
         debugPrint(sender.tag)
         
         let url = ImageURL[sender.tag]
-        let request = URLRequest(url: url)
-        
-        task = URLSession.shared.dataTask(with: request) { data, response, error in // 이미지 다운로드 끝나면 {} 실행
-            if let error = error {
-                guard error.localizedDescription == "cancelled" else {
-                    fatalError(error.localizedDescription)
-                }
-                DispatchQueue.main.async {
-                    self.reset()
-                }
-                return
-            }
-            // 여기까지 다 main thread에서 작업 (화면에서 조작 = main thread에서 작업)
-            guard let data = data, let image = UIImage(data: data) else {
-                DispatchQueue.main.async {
-                    // 이미지 다운 받고, 화면에 그리는 2개의 작업을 하기 때문에 동시성 사용, road all은 화면 5개를 그려야함
-                    // main을 통해서 이미지 다 다운 받았으니 화면에 그리라는 일을 전달 해야 하는 데 화면 UI 그리는일은 main thread에서만 가능하기 때문에 .main.async에서 imageView 그려주기
-                    self.imageView.image = .init(systemName: "xmark") // 일을 보낸쪽으로 다시 되돌려주어야하는 부분도 있기 때문에 되돌림
-                }
-                return
-            }
-            
-            DispatchQueue.main.async {
-                self.imageView.image = image
-                self.loadButton.isSelected = false
-            }
-        }
-    
-        observation = task.progress.observe(\.fractionCompleted, options: [.new], changeHandler:  { progress, change in
-            DispatchQueue.main.async {
-                self.progressView.progress = Float(progress.fractionCompleted)
-                // task의 진행상황 옵저빙(progress->fractionCompleted)
-            }
-        })
-        task.resume() // Datatask를 새로운 백그라운드 쓰레드에서 샐행시켜줌 -> 이미지 다운로드 작업 진행
+        startLoad(url: url)
     }
 }
